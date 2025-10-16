4e7137a8af5747f7caaeff1bbe61bda4f4069658
fernandofedora <noelonassis@gmail.com>
2025-10-15 21:12:17 -0500

    funtion basic class

--- 01-typescript-intro/src/topic/03-functions.ts
+++ 01-typescript-intro/src/topic/03-functions.ts
@@ -1,6 +1,19 @@
 
-function addNumbers() {
-    
+function addNumbers(a:number,b:number): number {
+    return a+b;
 }
 
+const addNumbersArrow = (a: number, b: number): string => {
+return `${a+b}`;
+}
+
+function multiply(fistnumer: number, secondNumeber?: number, base: number = 2) {
+    return fistnumer * base;
+}
+
+const result: number = addNumbers (1,2)
+const result2: string = addNumbersArrow(1,2);
+const multiplyResult: number = multiply(5)
+
+console.log({result, result2, multiplyResult})
 export {}
